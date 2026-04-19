1ab4700339bbd4b3c5eaaba4f57aa54767f1f40f
SzymonJedrzejczak <jedrzejczakbia@gmail.com>
2026-04-19 15:53:30 +0200

    Intial commit: intialize Spring Boot + Docker Compose

---
+++ backend/src/test/java/pl/diagnode/backend/TestBackendApplication.java
@@ -0,0 +1,11 @@
+package pl.diagnode.backend;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(BackendApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
