f8502326b2b1f45662b2728e8582a211d8be3b0c
SzymonJedrzejczak <jedrzejczakbia@gmail.com>
2026-04-23 21:22:59 +0200

    [DG-5] - tweaks

---
+++ backend/src/main/java/pl/diagnode/backend/domain/model/enums/NodeType.java
@@ -0,0 +1,12 @@
+package pl.diagnode.backend.domain.model.enums;
+
+public enum NodeType {
+
+    OPEN_QUESTION,
+    SINGLE_ANSWER_QUESTION,
+    MULTIPLE_ANSWER_QUESTION,
+    INTERLUDE,
+    AI_ANALYSIS,
+    END_OF_QUESTION_SET;
+    
+}
--- backend/src/main/java/pl/diagnode/backend/domain/repository/NodeRepository.java
+++ backend/src/main/java/pl/diagnode/backend/domain/repository/NodeRepository.java
@@ -1,6 +1,7 @@
 package pl.diagnode.backend.domain.repository;
 
 import org.jspecify.annotations.NonNull;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.diagnode.backend.domain.model.Node;
 
@@ -9,6 +10,7 @@ import java.util.UUID;
 
 public interface NodeRepository extends JpaRepository<@NonNull Node, @NonNull UUID> {
 
+    @Cacheable
     Optional<Node> findByNodeKey(String nodeKey);
 
 }
